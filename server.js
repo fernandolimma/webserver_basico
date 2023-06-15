24758f047ffa426bd5f23f125948c0d587253ac3
Fernando Lima <fernandoaslima@hotmail.com>
2023-06-15 10:53:55 -0300

    Primeiro Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// Criar um objeto http
+const http = require ('http');
+// Endereco da aplicação
+const hostname = '127.0.0.1';
+//Informar a porta da aplicação
+const port = 3000;
+
+// Chamando o objeto criado
+http.createServer ((req, res) =>{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("Hello World!");
+}).listen(port, hostname,() => {
+    console.log("O servidor esta rodando em http://127.0.0.1:3000/");
+});
